2ca80f097b4cf7822591e61c87340bc955b8c081
Stephen Oba <obastepheno@gmail.com>
2023-04-25 23:08:39 +0100

    🔧 Fix attribute error with babel.localeselector

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -7,31 +7,26 @@ from flask import request
 from flask_babel import Babel, _
 
 
-def get_locale():
-    """
-    Get User's locale from AcceptLanguagesx header
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale)
-
-
 class Config:
     """
-    Config for babel
+    Config for flask app
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-home_title = _("Welcome to Holberton")
-home_header = _("Hello World!")
+@babel.localeselector
+def get_locale():
+    """
+    Get User's locale from AcceptLanguagesx header
+    """
+    return "fr"
 
 
 @app.route('/')
@@ -39,7 +34,7 @@ def index():
     """
     Index view for app
     """
-    return render_template('3-index.html', title=home_title, header=home_header)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
