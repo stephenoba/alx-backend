6079ac6a740a74ade50e452a4fd6c3ee0d111278
Stephen Oba <obastepheno@gmail.com>
2023-04-25 16:37:05 +0100

    ➕ Add files 1-app.py and 1-index.html

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app that creates a single / route.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Config for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    Index view for app
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
